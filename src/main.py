74624d92cda5bbf81e763f28125c28279b3d956b
The-Real-Duke <andrey.shash.dik@gmail.com>
2025-09-22 21:56:55 +0500

    Refactor settings and hotkey logic and move to ui_settings.py

--- src/main.py
+++ src/main.py
@@ -5,9 +5,6 @@ import time
 import pystray
 import logging
 import asyncio
-import tkinter
-import tkinter.messagebox
-import keyboard
 import threading
 from PIL import Image
 from pathlib import Path
@@ -15,6 +12,7 @@ from playwright.async_api import async_playwright
 from playwright.__main__ import main as playwright_main
 from config import ConfigManager
 from devices import DeviceManager
+from ui_settings import SettingsWindowManager
 
 # Global Variables
 APPDATA_DIR = Path(os.getenv("APPDATA")) / "SHC"
@@ -25,7 +23,7 @@ device_counting_ready = asyncio.Event()
 loop = asyncio.new_event_loop()
 base_path = Path(os.path.abspath(".."))
 shc_image = base_path / "assets" / "icon.png"
-default_device = None
+default_device = config.get_info("CONFIG","default_device")
 executable_path = None
 inst = None
 browser = None
@@ -97,8 +95,8 @@ async def first_authorize():
     try:
         await page.wait_for_url(lambda url: "passport.yandex.ru" not in url, timeout=180000)
         logging.info("Authorization complete.")
-    except Exception:
-        logging.error("Timeout authorization")
+    except Exception as e:
+        logging.error(f"Timeout authorization:{e}")
         await browser.close()
         os._exit(1)
     await page.context.storage_state(path=cookies_path)
@@ -152,7 +150,7 @@ async def refresh_page(icon=None, item=None):
         await dm.d_count()
         update_shc_icon()
     except Exception as e:
-        logging.info("Can't refresh the page")
+        logging.info(f"Can't refresh the page:{e}")
 
 
 # Exiting program
@@ -169,197 +167,6 @@ async def exit_program(icon=None, item=None):
     await inst.stop()
     logging.info("Browser was closed")
     await loop.stop()
-
-
-# Hotkey functions
-def make_hotkey(current_hotkey):
-    keyboard.add_hotkey(
-        current_hotkey,
-        lambda: activate_hotkey()
-    )
-    config.set("CONFIG","dd_hotkey", str(current_hotkey))
-    config.save()
-    logging.info("Hotkey was maked")
-def remove_hotkey(current_hotkey):
-    keyboard.remove_hotkey(current_hotkey)
-    logging.info("Hotkey removed")
-def activate_hotkey():
-    logging.info("Detected hotkey")
-    asyncio.run_coroutine_threadsafe(activating_device(default_device), loop)
-
-
-# Setting window
-def settings_window():
-    global hotkey_window, current_hotkey, pressed_key_list, is_hotkey_exist
-    if not hotkey_window:
-        hotkey_window = tkinter.Tk()
-        hotkey_window.title("SHC Settings")
-        hotkey_window.eval("tk::PlaceWindow . center")
-        hotkey_window.geometry("270x180")
-        hotkey_window.iconphoto(True, tkinter.PhotoImage(file=shc_image))
-        hotkey_window.resizable(
-            width=False,
-            height=False
-        )
-        hotkey_frame = tkinter.Frame(
-            hotkey_window
-        )
-        hotkey_frame.place(
-            relx=0.05,
-            rely=0.01,
-            relwidth=0.9,
-            relheight=0.9)
-
-        def start_record():
-            global pressed_key_list
-            toggle_record_button.config(text="Stop record", command=lambda: stop_record())
-            title.config(text="Press wanted keys \n and press stop record (max 3 keys)")
-            pressed_key_list = []
-            hotkey_entry.config(state='normal')
-            hotkey_entry.delete(0, tkinter.END)
-            hotkey_entry.config(state='readonly')
-            keyboard.hook(pressed_keys)
-        def pressed_keys(event):
-            if len(pressed_key_list) < 3:
-                if event.name not in pressed_key_list:
-                    if event.event_type == "down":
-                        pressed_key_list.append(event.name)
-                        hotkey_entry.config(state='normal')
-                        hotkey_entry.delete(0, tkinter.END)
-                        hotkey_entry.insert(0, "+".join(pressed_key_list))
-                        hotkey_entry.config(state='readonly')
-            else:
-                stop_record()
-        def stop_record():
-            global pressed_key_list
-            keyboard.unhook_all()
-            toggle_record_button.config(text="Start record", command=lambda: start_record())
-            title.config(text="Press Apply to save hotkey \n or press Start record again")
-
-        def about():
-            tkinter.messagebox.showinfo(
-                title="SmartHomeControl",
-                message="SmartHomeControl aka SHC v1.0\nAuthor: The-Real-Duke\nLicense: CC BY-NC-SA 4.0\nSource: github.com/The-Real-Duke/SmartHomeControl"
-            )
-
-        def reset_current_keys():
-            global pressed_key_list, current_hotkey
-            if current_hotkey:
-                remove_hotkey(current_hotkey)
-            hotkey_entry.config(state='normal')
-            hotkey_entry.delete(0, tkinter.END)
-            hotkey_entry.config(state='readonly')
-            pressed_key_list = []
-            current_hotkey = ""
-            recorded_keys.config(text=f"Pressed keys: None")
-            current_hotkey_label.config(text=f"Current hotkey: {current_hotkey if current_hotkey else "None"}")
-
-        def apply_keys():
-            global pressed_key_list, current_hotkey
-            if not pressed_key_list:
-                return
-            if is_hotkey_exist:
-                remove_hotkey(current_hotkey)
-            current_hotkey = "+".join(pressed_key_list)
-            make_hotkey(current_hotkey)
-            title.config(text="Applied. \n Press start record to set the another hotkey.")
-            current_hotkey_label.config(text=f"Current hotkey: {current_hotkey}")
-
-        apply_button = tkinter.Button(
-            hotkey_frame,
-            text="Apply",
-            command=lambda: apply_keys()
-        )
-        apply_button.place(
-            rely=0.6,
-            relx=0,
-            relwidth=0.35
-        )
-        about_button = tkinter.Button(
-            hotkey_frame,
-            text="About",
-            command=lambda: about()
-        )
-        about_button.place(
-            rely=0.8,
-            relx=0,
-            relwidth=0.35
-        )
-        toggle_record_button = tkinter.Button(
-            hotkey_frame,
-            text="Start record",
-            command=lambda: start_record()
-        )
-        toggle_record_button.place(
-            rely=0.4,
-            relx=0,
-            relwidth=0.35
-        )
-        title = tkinter.Label(
-            hotkey_frame,
-            text="Press start record to set the hotkey."
-        )
-        title.place(
-            relheight=0.19, relwidth=1
-        )
-        current_hotkey_label = tkinter.Label(
-            hotkey_frame,
-            text=f"Current hotkey: {current_hotkey}"
-        )
-        current_hotkey_label.place(
-            rely=0.21, relx=0.37
-        )
-        recorded_keys = tkinter.Label(
-            hotkey_frame,
-            text=f"Pressed keys:"
-        )
-        recorded_keys.place(
-            rely=0.35, relx=0.37
-        )
-        hotkey_entry = tkinter.Entry(
-            hotkey_frame,
-            bg="white",
-            state='readonly'
-        )
-        hotkey_entry.place(
-            rely=0.5,
-            relx=0.385,
-            relwidth=0.5,
-            relheight=0.15)
-        reset_button = tkinter.Button(
-            hotkey_frame,
-            text="Reset",
-            command=lambda: reset_current_keys()
-        )
-        reset_button.place(
-            rely=0.2,
-            relx=0,
-            relwidth=0.35
-        )
-        def on_close():
-            global hotkey_window
-            hotkey_window.destroy()
-            hotkey_window = None
-        hotkey_window.protocol("WM_DELETE_WINDOW", on_close)
-        hotkey_window.mainloop()
-
-    else:
-        hotkey_window.deiconify()
-        hotkey_window.lift()
-        hotkey_window.focus_force()
-
-
-# Hotkeys stop working after windows locks & unlocks
-# https://github.com/boppreh/keyboard/issues/223
-# Thanks that man 2-3-5-7
-def clear_keys():
-    while True:
-        with keyboard._pressed_events_lock:
-            for i in list(keyboard._pressed_events.keys()):
-                item = keyboard._pressed_events[i]
-                if time.time() - item.time > 2:
-                    del keyboard._pressed_events[i]
-        time.sleep(2)
 
 
 #Click trigger
@@ -412,7 +219,7 @@ def build_shc_tray_icon():
                 )
             ),
         pystray.MenuItem(
-            "Settings", settings_window
+            "Settings", lambda icon, item: settings_window.open()
         ),
         pystray.MenuItem(
             "Refresh Page", lambda icon, item: asyncio.run_coroutine_threadsafe(refresh_page(),loop)
@@ -424,7 +231,7 @@ def build_shc_tray_icon():
     )
 
 
-#Updating tray (for some reason works with big latency)
+#Updating tray
 def update_shc_icon():
     shc_tray_icon.menu=build_shc_tray_icon()
     shc_tray_icon.update_menu()
@@ -437,7 +244,8 @@ def start_shc_icon():
     shc_tray_icon = pystray.Icon("SmartHomeControl", icon=Image.open(shc_image), title="SmartHomeControl", menu=build_shc_tray_icon())
     logging.info("Starting tray")
     shc_tray_icon.run()
-
+def activating_dd():
+    asyncio.run_coroutine_threadsafe(activating_device(default_device), loop)
 
 # Background updater each 15 min + random seconds for antibot system
 async def background_updater():
@@ -445,6 +253,7 @@ async def background_updater():
         await asyncio.sleep(3600 + random.randint(-240, 240))
         await refresh_page()
 
+settings_window = SettingsWindowManager(shc_image, config, activating_dd)
 # Main
 async def main():
     logging.info("Starting tray initialization...")
@@ -458,7 +267,7 @@ async def main():
     await device_counting_ready.wait()
     logging.info("Updating tray...")
     update_shc_icon()
-    threading.Thread(target=clear_keys, daemon=True).start()
+    threading.Thread(target=SettingsWindowManager.clear_keys, daemon=True).start()
     asyncio.create_task(background_updater())
 
 
---
+++ src/ui_settings.py
@@ -0,0 +1,196 @@
+import time
+import tkinter
+import tkinter.messagebox
+import keyboard
+
+class SettingsWindowManager:
+    def __init__(self, image, config, on_hotkey_callback):
+        self.image = image
+        self.window = None
+        self.config = config
+        self.current_hotkey = self.config.get_info("CONFIG", "dd_hotkey")
+        if self.current_hotkey != "":
+            self._make_hotkey()
+        self.on_hotkey_callback = on_hotkey_callback
+        self.pressed_key_list = []
+
+    def open(self):
+        if not self.window:
+            self.window = tkinter.Tk()
+            self.window.title("SHC Settings")
+            self.window.eval("tk::PlaceWindow . center")
+            self.window.geometry("270x180")
+            self.window.iconphoto(True, tkinter.PhotoImage(file=self.image))
+            self.window.resizable(
+                width=False,
+                height=False
+            )
+            self.hotkey_frame = tkinter.Frame(
+                self.window
+            )
+            self.hotkey_frame.place(
+                relx=0.05,
+                rely=0.01,
+                relwidth=0.9,
+                relheight=0.9)
+            self.apply_button = tkinter.Button(
+                self.hotkey_frame,
+                text="Apply",
+                command=lambda: self._apply_keys()
+            )
+            self.apply_button.place(
+                rely=0.6,
+                relx=0,
+                relwidth=0.35
+            )
+            self.about_button = tkinter.Button(
+                self.hotkey_frame,
+                text="About",
+                command=lambda: self._about()
+            )
+            self.about_button.place(
+                rely=0.8,
+                relx=0,
+                relwidth=0.35
+            )
+            self.toggle_record_button = tkinter.Button(
+                self.hotkey_frame,
+                text="Start record",
+                command=lambda: self._start_record()
+            )
+            self.toggle_record_button.place(
+                rely=0.4,
+                relx=0,
+                relwidth=0.35
+            )
+            self.title = tkinter.Label(
+                self.hotkey_frame,
+                text="Press start record to set the hotkey."
+            )
+            self.title.place(
+                relheight=0.19, relwidth=1
+            )
+            self.current_hotkey_label = tkinter.Label(
+                self.hotkey_frame,
+                text=f"Current hotkey: {self.current_hotkey}"
+            )
+            self.current_hotkey_label.place(
+                rely=0.21, relx=0.37
+            )
+            self.recorded_keys = tkinter.Label(
+                self.hotkey_frame,
+                text=f"Pressed keys:"
+            )
+            self.recorded_keys.place(
+                rely=0.35, relx=0.37
+            )
+            self.hotkey_entry = tkinter.Entry(
+                self.hotkey_frame,
+                bg="white",
+                state='readonly'
+            )
+            self.hotkey_entry.place(
+                rely=0.5,
+                relx=0.385,
+                relwidth=0.5,
+                relheight=0.15)
+            self.reset_button = tkinter.Button(
+                self.hotkey_frame,
+                text="Reset",
+                command=lambda: self._reset_current_keys()
+            )
+            self.reset_button.place(
+                rely=0.2,
+                relx=0,
+                relwidth=0.35
+            )
+            self.window.protocol("WM_DELETE_WINDOW", self._on_close)
+            self.window.mainloop()
+        else:
+            self.window.deiconify()
+            self.window.lift()
+            self.window.focus_force()
+
+    def _on_close(self):
+        self.window.destroy()
+        self.window = None
+
+    def _make_hotkey(self):
+        keyboard.add_hotkey(
+            self.current_hotkey,
+            lambda: self.on_hotkey_callback()
+        )
+        if self.window:
+            self.config.set("CONFIG", "dd_hotkey", str(self.current_hotkey))
+            self.config.save()
+
+    def _start_record(self):
+        self.toggle_record_button.config(text="Stop record", command=lambda: self._stop_record())
+        self.title.config(text="Press wanted keys \n and press stop record (max 3 keys)")
+        self.pressed_key_list = []
+        self.hotkey_entry.config(state='normal')
+        self.hotkey_entry.delete(0, tkinter.END)
+        self.hotkey_entry.config(state='readonly')
+        keyboard.hook(self._pressed_keys)
+
+    def _pressed_keys(self, event):
+        if len(self.pressed_key_list) < 3:
+            if event.name not in self.pressed_key_list:
+                if event.event_type == "down":
+                    self.pressed_key_list.append(event.name)
+                    self.hotkey_entry.config(state='normal')
+                    self.hotkey_entry.delete(0, tkinter.END)
+                    self.hotkey_entry.insert(0, "+".join(self.pressed_key_list))
+                    self.hotkey_entry.config(state='readonly')
+        else:
+            self._stop_record()
+
+    def _stop_record(self):
+        keyboard.unhook(self._pressed_keys)
+        self.toggle_record_button.config(text="Start record", command=lambda: self._start_record())
+        self.title.config(text="Press Apply to save hotkey \n or press Start record again")
+
+    def _reset_current_keys(self):
+        if self.current_hotkey:
+            self._remove_hotkey(self.current_hotkey)
+        self.hotkey_entry.config(state='normal')
+        self.hotkey_entry.delete(0, tkinter.END)
+        self.hotkey_entry.config(state='readonly')
+        self.pressed_key_list = []
+        self.current_hotkey = ""
+        self.config.set("CONFIG", "dd_hotkey", str(self.current_hotkey))
+        self.config.save()
+        self.recorded_keys.config(text=f"Pressed keys:")
+        self.current_hotkey_label.config(text=f"Current hotkey: {self.current_hotkey if self.current_hotkey else "None"}")
+
+    def _apply_keys(self):
+        if not self.pressed_key_list:
+            return
+        if self.current_hotkey:
+            self._remove_hotkey(self.current_hotkey)
+        self.current_hotkey = "+".join(self.pressed_key_list)
+        self._make_hotkey()
+        self.title.config(text="Applied. \n Press start record to set the another hotkey.")
+        self.current_hotkey_label.config(text=f"Current hotkey: {self.current_hotkey}")
+        print(self.current_hotkey)
+
+    @staticmethod
+    def _remove_hotkey(keys):
+        keyboard.remove_hotkey(keys)
+
+    @staticmethod
+    def _about():
+        tkinter.messagebox.showinfo(
+            title="SmartHomeControl",
+            message="SmartHomeControl aka SHC v1.0\nAuthor: The-Real-Duke\nLicense: CC BY-NC-SA 4.0\nSource: github.com/The-Real-Duke/SmartHomeControl"
+        )
+
+    @staticmethod
+    def clear_keys():
+        while True:
+            with keyboard._pressed_events_lock:
+                for i in list(keyboard._pressed_events.keys()):
+                    item = keyboard._pressed_events[i]
+                    if time.time() - item.time > 2:
+                        del keyboard._pressed_events[i]
+            time.sleep(5)
